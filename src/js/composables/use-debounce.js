64a463173b6c083a649f4e10a67ca20a5b8098b6
sergey-uhanov <uhanov.sergio@gmail.com>
2025-09-25 14:27:32 +0300

    serhii-dev: delete comments

--- src/js/composables/use-debounce.js
+++ src/js/composables/use-debounce.js
@@ -1,16 +1,3 @@
-/**
- * Creates and returns a debounced version of the provided function.
- * The debounced function delays the execution of `func` until after `wait` milliseconds
- * have elapsed since the last time it was invoked.
- *
- * Optionally, if `immediate` is `true`, the function is triggered on the leading edge
- * instead of the trailing.
- *
- * @param {Function} func - The function to debounce.
- * @param {number} wait - The number of milliseconds to delay.
- * @param {boolean} [immediate=false] - If true, triggers the function on the leading edge.
- * @returns {Function} A debounced version of the original function.
- */
 export const useDebounce = (func, wait, immediate) => {
   let timeout
 
